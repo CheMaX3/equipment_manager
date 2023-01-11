4bfee9b8a530e928208c014453b4b1f69973083e
MMetlin <MMetlin@inno.tech>
2023-01-11 14:55:16 +0300

    Delete wrong module

--- project/src/main/java/com/chemax/project/exceptions/EntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.chemax.project.exceptions;
-
-import java.util.function.Supplier;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException () {
-        System.out.println("Объект с указанным ID не найден");
-    }
-}
--- project/src/main/java/com/chemax/project/repository/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chemax.project.repository;
-
-import com.chemax.project.request.SectionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<SectionEntity, Integer> {
-
-}
