e70c16322ae031f04aa1b4694b280237a36c5e30
Max Vorobyev <chemax05@mail.ru>
2023-02-01 15:03:25 +0300

    Deleted unused classes and dependencies

--- src/main/java/com/chemax/project/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.chemax.project.repository;
-//
-//import com.chemax.project.entities.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserRepository extends JpaRepository <User, Integer> {
-//    User findByUsername(String username);
-//}
