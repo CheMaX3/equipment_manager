4199386bab3084509506c4331d443b3657d47b04
CheMaX <chemax05@mail.ru>
2023-02-02 20:18:31 +0300

    Solved problem circular references

--- src/main/java/com/chemax/project/entities/Role.java
+++ src/main/java/com/chemax/project/entities/Role.java
@@ -1,38 +1,38 @@
-//package com.chemax.project.entities;
-//
-//import lombok.Data;
-//import org.springframework.security.core.GrantedAuthority;
-//
-//import javax.persistence.*;
-//import java.util.Set;
-//
-//@Entity
-//@Data
-//@Table (name = "role")
-//public class Role implements GrantedAuthority {
-//
-//    @Id
-//    private Integer id;
-//    private String name;
-//
-//    @Transient
-//    @ManyToMany (mappedBy = "roles")
-//    private Set<User> users;
-//
-//    public Role() {
-//    }
-//
-//    public Role(Integer id) {
-//        this.id = id;
-//    }
-//
-//    public Role(Integer id, String name) {
-//        this.id = id;
-//        this.name = name;
-//    }
-//
-//    @Override
-//    public String getAuthority() {
-//        return getName();
-//    }
-//}
+package com.chemax.project.entities;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
+@Table (name = "role")
+public class Role implements GrantedAuthority {
+
+    @Id
+    private Integer id;
+    private String name;
+
+    @Transient
+    @ManyToMany (mappedBy = "roles")
+    private Set<User> users;
+
+    public Role() {
+    }
+
+    public Role(Integer id) {
+        this.id = id;
+    }
+
+    public Role(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+}
