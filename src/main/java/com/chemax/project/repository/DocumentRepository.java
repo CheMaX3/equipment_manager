dd59c237768c5a45f10152cd99b299233c0eb4f9
CheMaX <chemax05@mail.ru>
2023-02-15 16:21:08 +0300

    Trying to add MongoDB

---
+++ src/main/java/com/chemax/project/repository/DocumentRepository.java
@@ -0,0 +1,8 @@
+//package com.chemax.project.repository;
+//
+//import com.chemax.project.entities.Document;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//
+//public interface DocumentRepository extends MongoRepository<Document, String> {
+//    public Document findByName(String name);
+//}
